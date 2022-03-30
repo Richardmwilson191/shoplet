39067fe994c6061c71c252db199f5bcdbe42b178
Richard Wilson <richardmwilson191@gmail.com>
2022-03-29 23:22:22 -0500

    First commit, created some files, migrations and product category REST endpoints

---
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreServiceCategoryRequest;
+use App\Http\Requests\UpdateServiceCategoryRequest;
+use App\Models\ServiceCategory;
+
+class ServiceCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ServiceCategory::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreServiceCategoryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreServiceCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ServiceCategory  $serviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ServiceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateServiceCategoryRequest  $request
+     * @param  \App\Models\ServiceCategory  $serviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateServiceCategoryRequest $request, ServiceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ServiceCategory  $serviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ServiceCategory $serviceCategory)
+    {
+        //
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_category_id',
+        'prod_nm',
+        'desc',
+        'price',
+        'img_path',
+        'condition',
+        'sold_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
+}
